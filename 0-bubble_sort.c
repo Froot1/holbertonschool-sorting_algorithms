a38739839e84b0ac185611b5872bfefedf025c9c
root <10679@holbertonstudents.com>
2025-04-11 11:33:33 +0000

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,48 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorting algorithms
- * @array: array
- * @size: size
+ * swap_bubble - function for sorting with swap method.
+ *@a: element to be swapped.
+ *@b: element to be swapped.
+ * Return: nothing.
  */
+
+void swap_bubble(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - sorts an array using bubble sort algorithm.
+ *@array: array to be sorted.
+ *@size: size to be sorted.
+ * Return: void.
+ */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int tmp;
+	size_t i, sz = size;
+	bool booln = false;
 
-	for (i = 0; i < size; i++)
+	if (array == NULL || size < 2)
+		return;
+
+	while (booln == false)
 	{
-		for (j = 0; j < (size - i - 1); j++)
+		booln = true;
+		for (i = 0; i < sz - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-
+				swap_bubble(array + i, array + i + 1);
 				print_array(array, size);
+				booln = false;
 			}
 		}
+		sz--;
 	}
 }
