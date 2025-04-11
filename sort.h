8509269eb740f08fea0a53b1cb7ab35b8937b49a
root <10679@holbertonstudents.com>
2025-04-11 12:48:02 +0000

    update

--- sort.h
+++ sort.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
 /**
  * enum bool - Enumeration of Boolean values.
  * @false: Equals 0.
